ee33c54badd843e076d6d13e29051b8473172d47
marcel <marcel@finsera.com>
2021-11-17 17:24:59 -0300

    Adicionado suporte para API

---
+++ lib/models/FaleConoscoClass.dart
@@ -0,0 +1,9 @@
+class FaleConoscoClass {
+  final String nome;
+  final String email;
+  final String telefone;
+  final String assunto;
+  final String mensagem;
+
+  FaleConoscoClass({this.nome, this.email, this.telefone, this.assunto, this.mensagem});
+}
